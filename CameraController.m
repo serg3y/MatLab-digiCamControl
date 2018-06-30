9c77b4f2849f8135058b482cc0e35c051d2fa574
Serge <s3rg3y@hotmail.com>
2018-07-01 03:26:35 +0930

    comments changes

--- CameraController.m
+++ CameraController.m
@@ -2,16 +2,16 @@
 % C = CameraController    -create class and auto-detect digiCamControl
 % C = CameraController(ip)  -address of pc running digiCamControl webserver
 % C = CameraController(fold)  -folder containing digiCamControl app
-%
+% 
 %Instruction:
 %1.Install and run digiCamControl, BETA v2.0.69 or greater, from:
 %  https://sourceforge.net/projects/digicamcontrol/files/latest/download
-%2.Enable webserver: File>Settings>Webserver>Enable **RESTART REQUIRED**.
+%2.Enable webserver: File>Settings>Webserver>Enable>RESTART APP.
 %3.Connect one or more cameras using USB cable (or WiFi if supported).
-%4.For full manual control set camera mode to (M) and lens to (MF).
-%5.Test the connection through digiCamControl GUI.
-%6.Try the examples and read this help.
-%
+%4.For full control set camera to (M) and lens to (MF).
+%5.Ensure camera is working through digiCamControl.
+%6.Try the examples bellow and read this help.
+% 
 %Remarks:
 %-This class can be used to control supported cameras, stream liveview,
 % capture photos & video, download captured files, change settings such as
@@ -26,7 +26,7 @@
 %-Method in this class are Capitalised and have additional descriptions.
 %-When this class is created it does a one of retrieval of allowed camera
 % options. Redefine this class when swapping cameras.
-%
+% 
 %Limitations:
 %-This class cannot download old photos, user has to use digiCamControl app
 % manually: <a href=http://digicamcontrol.com/doc/userguide/interface/downph>digiCamControl>Download photos</a>. 
@@ -38,7 +38,7 @@
 % being removed. These properties can be queried but can not be set. 
 % eg "-", "." in "center-weighted_area" "active_d-lighting" "long_exp._nr"
 %-digiCamControl issues: http://digicamcontrol.com/phpbb/viewforum.php?f=4
-%
+% 
 %Camera Settings:
 %-Some settings will not have affect if camera is not in Manual mode (M).
 %-To control focus ensure lens is set to Manual Focus (MF):
@@ -46,82 +46,77 @@
 %-Note: Lenses use servo motors which have no discrete physical 'steps'. To
 % achieve a specific focus reproducibly try to go to the lens's physical
 % limit, in either direction, and apply a set change from there.
-%
+% 
 %Image Capture:
-%-To reduce capture latency from 0.3-0.6 sec to ~0.05s enable HTTP
-% webserver, File>Settings>Webserver *RESTART IS REQUIRED*
-%-To measure the delay and variance try imaging the computer's own clock by
-% calling the "Clock" method provided with this class, ie C.Clock
+%-To reduce capture latency from 0.3-0.6 sec to ~0.05s ensure webserver is
+% enabled, File>Settings>Webserver>Enable>RESTART APP
+%-To measure delay and variance try imaging the computer's own clock by
+% calling the "Clock" method provided with this class, C.Clock, however
+% I do not know how to measure monitor display latency and variance.
 %-Cmd('CaptureAll') will trigger all connected cameras but there will be a
 % lag of 0.005-0.020 sec between consecutive cameras.
 %-To record video turn on live preview using Cmd('LiveViewWnd_Show') and
 % user Cmd('StartRecord') and Cmd('StopRecord').
 %-The Capture method blocks code except, but only if acquisition + download
 % take more then ~3 sec, then digiCamControl returns without error.
-%
-%Download settings:
-%-Download is affected by: 1) Transfer mode, 2) session settings
-%-Transfer mode can ONLY be set via the GUI: "PC Only"|"Cam Only"|"PC+Cam".
-%-Transfer mode restricts where photos are allowed to be saved and session
-% settings can further restrict those limits, but not expand them.
-%RECOMMENDATION:use "PC+Cam"+"deletefileaftertransfer" instead of "PC only"
-%-Some session settings only work under certain conditions:
-% "downloadonlyjpg" prevents RAWs from downloading only if mode="PC+Cam"
-% "deletefileaftertransfer" only works if mode="PC+Cam"
-% "useoriginalfilename" only works when mode="PC+Cam" 
-% "downloadthumbonly" never works (v2.0.72.9)
-% "filenametemplate" only works when "useoriginalfilename" is off
-%-"filenametemplate" is only applied to downloaded files, not camera files.
-%-"filenametemplate" supports many useful [tags], eg: [Date yyyy-MM-dd],
-% [Time hh-mm-ss], [Date yyyy-MM-dd-hh-mm-ss], [Exif.Photo.ExposureTime],
-% [Exif.Photo.FNumber], [Exif.Photo.ISOSpeedRatings], etc
+% 
+%Image Download:
+%-Download is affected by Transfer mode (in app) and session settings.
+%-Transfer mode is set via the main app to: PC & Camera | Camera only, if
+% set to Camera only some session settings will be ignored. Set it to PC
+% & Camera and use session setting "deletefileaftertransfer" if needed.
+%-session setting "filenametemplate" works only if "useoriginalfilename" is
+% disabled, and it is applied to downloaded files only, not camera files.
+% It supports many useful [tags], eg: [Date yyyy-MM-dd], [Time hh-mm-ss],
+% [Date yyyy-MM-dd-hh-mm-ss], [Exif.Photo.ExposureTime],
+% [Exif.Photo.FNumber], [Exif.Photo.ISOSpeedRatings], etc 
 % (for a full list go to: Session>Edit Current Session>File Name Template)
-%-"filenametemplate" can be set when calling the Capture method.
-%-"filenametemplate" applies to all connected cameras, to make sure files
-% are downloaded with different filenames use [Camera Name], [Camera Counter 4 digit]
-%-"folder" does not support [tags], so use a "\" in the filename instead.
-%
+%-"filenametemplate" can be set when calling the Capture method and applies
+% to all connected cameras. To distinguish cameras use [Camera Name] or
+% [Camera Counter 4 digit] tags in the template.
+%-"folder" does not support [tags], instead use "\" in "filenametemplate".
+% 
 %Ex: download settings, see also "Transfer" in bottom left of GUI
 % C = CameraController;
-% C.session.folder                  = 'C:\DSLR';
-% C.session.filenametemplate        = '[Camera Name]\[Date yyyy-MM-dd-hh-mm-ss]';
-% C.session.useoriginalfilename     = 0; %ignores "filenametemplate"
-% C.session.downloadthumbonly       = 0; %not working (v2.0.72.9)
-% C.session.downloadonlyjpg         = 0; %only used if "PC+CAM"
+% C.session.folder = 'C:\DSLR';
+% C.session.filenametemplate = '[Camera Name]\[Date yyyy-MM-dd-hh-mm-ss]';
+% C.session.useoriginalfilename = 0; %ignores "filenametemplate"
+% C.session.downloadthumbonly = 0; %not working (v2.0.72.9)
+% C.session.downloadonlyjpg = 0; %only used if "PC+CAM"
 % C.session.deletefileaftertransfer = 1; %only has affect if Transfer="Cam+PC" and affectively converts it to "PC only"
-% C.session.asksavepath             = 0; %dialog popup for after capture
-% C.session.allowoverwrite          = 0; %overwrite if file exists
-% C.session.lowercaseextension      = 1; %use "*.jpg" instead of "*.JPG"
-%
-%Ex: basic camera settings
+% C.session.asksavepath = 0; %dialog popup for after capture
+% C.session.allowoverwrite = 0; %overwrite if file exists
+% C.session.lowercaseextension = 1; %use "*.jpg" instead of "*.JPG"
+% 
+%Ex: camera settings
 % C = CameraController;
-% C.camera.isonumber          = 100;
-% C.camera.fnumber            = 4;
-% C.camera.shutterspeed       = 1/200;
+% C.camera.isonumber = 100;
+% C.camera.fnumber = 4;
+% C.camera.shutterspeed = 1/200;
 % C.camera.compressionsetting = 'Large Fine JPEG';
-% C.camera.drive_mode         = 'Single-Frame Shooting';
-%
+% C.camera.drive_mode = 'Single-Frame Shooting';
+% 
 %Ex: simple capture
-% C = CameraController;        %initialise
-% C.Capture                    %capture (filename set by "session.filenametemplate") 
-% C.Capture('MyPhoto')         %capture (set custom filename)
+% C = CameraController; %initialise
+% C.Capture %capture (filename set by "session.filenametemplate") 
+% C.Capture('MyPhoto') %capture (set custom filename)
 % C.Capture('[Time hh-mm-ss]') %capture (use time tag as filename)
-% file = C.lastfile            %get last downloaded filenames
-%
+% file = C.lastfile %get last downloaded filenames
+% 
 %Ex: timed capture
 % C = CameraController;
 % time = ceil(now*24*60*6)/24/60/60; %upcoming whole second
 % file = [datestr(time,'yyyy-mm-dd_HHMMSS.FFF') '_' C.property.devicename]; %timestamp & camera name
 % C.Capture(file,time); %capture
 % datestr(time)
-%
+% 
 %Ex: two cameras
 % C = CameraController;
-% C.session.filenametemplate = 'MyTest_[Camera Name]'; %set filename pattern
-% C.Cameras(1), C.property.devicename = 'Cam1'; %set camera name (optional)
+% C.session.filenametemplate = '[Camera Name]'; %set filename pattern
+% C.Cameras(1), C.property.devicename = 'Cam1'; %change camera name
 % C.Cameras(2), C.property.devicename = 'Cam2';
 % C.Cmd('CaptureAll')
-%
+% 
 %Ex: focus stacking
 % C = CameraController;
 % C.Cmd('LiveViewWnd_Show'), pause(1)  %turn on live preview
@@ -130,28 +125,28 @@
 %     C.Capture(num2str(k,'Focus%g')); %capture and number the photos
 % end
 % C.Cmd('LiveViewWnd_Hide')            %turn off live preview to save battery
-%
+% 
 %Ex: stream live view
-%To remove rectangle:  Live View>Display>Show focus rectangle
+%To remove rectangle: Live View>Display>Show focus rectangle
 %To reduce lag enable: Live View>Display>No processing
 % C = CameraController;
-% C.Cmd('LiveViewWnd_Show');                    %start live view
-% C.Cmd('All_Minimize');                        %minimise digiCamControl
-% pause(3)                                      %wait for live view
-% clf, h = imshow(C.LiveView);                  %prepare figure
+% C.Cmd('LiveViewWnd_Show'); %start live view
+% C.Cmd('All_Minimize'); %minimise digiCamControl
+% pause(3) %wait for live view
+% clf, h = imshow(C.LiveView); %prepare figure
 % uicontrol('str','Capture','call','C.Capture') %capture button
-% while ishandle(h)                             %loop until closed
-%     set(h,'cdata',C.LiveView)                 %update live view
-%     drawnow                                   %update display
+% while ishandle(h) %loop until closed
+%     set(h,'cdata',C.LiveView) %update live view
+%     drawnow %update display
 % end
-% C.Cmd('LiveViewWnd_Hide');                    %stop live view
+% C.Cmd('LiveViewWnd_Hide'); %stop live view
 % 
 %Ex: debugging
 % C = CameraController;
-% C.Clock      %show a clock with milliseconds
-% C.dbg = 2;   %display commands and replies
-% C.Capture    %capture photo
-%
+% C.Clock %show a clock with milliseconds
+% C.dbg = 2; %display commands and replies
+% C.Capture %capture photo
+% 
 %Serge 2017
 % Questions/bugs/fixes: <a href="http://mailto:s3rg3y@hotmail.com">s3rg3y@hotmail.com</a>
  
@@ -164,13 +159,10 @@
 %-Allow commas in filenames
 %-Minor changes and better help
  
-%Update ReadMe:
+%Update ReadMe.txt:
 % fprintf(fopen('ReadMe.txt','w'),'%s',regexprep(help('CameraController'),{'<a .*?>|</a>' '\n ' ' *' '\n\n'},{'' '\n' ' ' '\n \n'}));fclose all
  
-%Latest beta:
-% https://s3.amazonaws.com/download.digicamcontrol.com/digiCamControlsetup_2.0.72.9.exe (2017-06-19) 
- 
-%Example HTTP webserver commands, use urlread(url)
+%Example webserver commands:
 % http://localhost:5513                                                     %primitive http GUI 
 % http://localhost:5513/?SLC=CaptureNoAf&param1=Test\[Time%20hh-mm-ss]      %capture and set filename
 % http://localhost:5513/?CMD=Capture                                        %capture and display controls webpage with currently selected (previous) photo
@@ -191,11 +183,10 @@
 % http://localhost:5513/?SLC=Set&param1=session.folder&param2=c:\pictures
 % http://localhost:5513/?SLC=Set&param1=session.filenametemplate&param2=capture1
  
-%Can use CameraControlCmd.exe when digiCamControl is off! VERY SLOW!
-% http://digicamcontrol.com/doc/userguide/cmd
-%Examples:
+%Example CameraControlCmd.exe commands: work when digiCamControl is off, but very SLOW!
 % system('"C:\Program Files (x86)\digiCamControl\CameraControlCmd.exe" /filename E:\test\test.jpg /capture')
 % system('"C:\Program Files (x86)\digiCamControl\CameraControlCmd.exe" /captureallnoaf')
+%See also: http://digicamcontrol.com/doc/userguide/cmd
 %Applies to all cameras:
 % /help                      - this screen
 % /capture                   - capture photo
@@ -223,19 +214,6 @@
 % /comment comment           - set in camera comment string 
 % /copyright copyright       - set in camera copyright string 
 % /artist artist             - set in camera artist string 
- 
-%ERROR "Object does not match target type." 
-% C.property.nodownload = 0; C.property.captureinsdram = 0; %PC+CAM 
-% C.property.nodownload = 1; C.property.captureinsdram = 0; %CAM 
-% C.property.nodownload = 1; C.property.captureinsdram = 1; %PC
- 
-%Can use dll files directly using NET.addAssembly, did not work for me...
-% https://github.com/JonHoy/Matlab_DSLR_Camera_Control/
-%>> camera.CapturePhoto();
-% Error using CameraControl_MATLAB (line 20)
-% Message: The method or operation is not implemented.
-% Source: CameraControl.Devices
-% HelpLink: 
  
 classdef CameraController < handle
     %% Properties
--- ReadMe.txt
+++ ReadMe.txt
@@ -6,11 +6,11 @@
 Instruction:
 1.Install and run digiCamControl, BETA v2.0.69 or greater, from:
  https://sourceforge.net/projects/digicamcontrol/files/latest/download
-2.Enable webserver: File>Settings>Webserver>Enable **RESTART REQUIRED**.
+2.Enable webserver: File>Settings>Webserver>Enable>RESTART APP.
 3.Connect one or more cameras using USB cable (or WiFi if supported).
-4.For full manual control set camera mode to (M) and lens to (MF).
-5.Test the connection through digiCamControl GUI.
-6.Try the examples and read this help.
+4.For full control set camera to (M) and lens to (MF).
+5.Ensure camera is working through digiCamControl.
+6.Try the examples bellow and read this help.
  
 Remarks:
 -This class can be used to control supported cameras, stream liveview,
@@ -48,38 +48,17 @@ Camera Settings:
  limit, in either direction, and apply a set change from there.
  
 Image Capture:
--To reduce capture latency from 0.3-0.6 sec to ~0.05s enable HTTP
- webserver, File>Settings>Webserver *RESTART IS REQUIRED*
--To measure the delay and variance try imaging the computer's own clock by
- calling the "Clock" method provided with this class, ie C.Clock
+-To reduce capture latency from 0.3-0.6 sec to ~0.05s ensure webserver is
+ enabled, File>Settings>Webserver>Enable>RESTART APP
+-To measure delay and variance try imaging the computer's own clock by
+ calling the "Clock" method provided with this class, C.Clock, however
+ I do not know how to measure monitor display latency and variance.
 -Cmd('CaptureAll') will trigger all connected cameras but there will be a
  lag of 0.005-0.020 sec between consecutive cameras.
 -To record video turn on live preview using Cmd('LiveViewWnd_Show') and
  user Cmd('StartRecord') and Cmd('StopRecord').
 -The Capture method blocks code except, but only if acquisition + download
  take more then ~3 sec, then digiCamControl returns without error.
- 
-Download settings:
--Download is affected by: 1) Transfer mode, 2) session settings
--Transfer mode can ONLY be set via the GUI: "PC Only"|"Cam Only"|"PC+Cam".
--Transfer mode restricts where photos are allowed to be saved and session
- settings can further restrict those limits, but not expand them.
-RECOMMENDATION:use "PC+Cam"+"deletefileaftertransfer" instead of "PC only"
--Some session settings only work under certain conditions:
- "downloadonlyjpg" prevents RAWs from downloading only if mode="PC+Cam"
- "deletefileaftertransfer" only works if mode="PC+Cam"
- "useoriginalfilename" only works when mode="PC+Cam" 
- "downloadthumbonly" never works (v2.0.72.9)
- "filenametemplate" only works when "useoriginalfilename" is off
--"filenametemplate" is only applied to downloaded files, not camera files.
--"filenametemplate" supports many useful [tags], eg: [Date yyyy-MM-dd],
- [Time hh-mm-ss], [Date yyyy-MM-dd-hh-mm-ss], [Exif.Photo.ExposureTime],
- [Exif.Photo.FNumber], [Exif.Photo.ISOSpeedRatings], etc
- (for a full list go to: Session>Edit Current Session>File Name Template)
--"filenametemplate" can be set when calling the Capture method.
--"filenametemplate" applies to all connected cameras, to make sure files
- are downloaded with different filenames use [Camera Name], [Camera Counter 4 digit]
--"folder" does not support [tags], so use a "\" in the filename instead.
  
 Ex: download settings, see also "Transfer" in bottom left of GUI
  C = CameraController;
@@ -93,7 +72,7 @@ Ex: download settings, see also "Transfer" in bottom left of GUI
  C.session.allowoverwrite = 0; %overwrite if file exists
  C.session.lowercaseextension = 1; %use "*.jpg" instead of "*.JPG"
  
-Ex: basic camera settings
+Ex: camera settings
  C = CameraController;
  C.camera.isonumber = 100;
  C.camera.fnumber = 4;
@@ -108,49 +87,13 @@ Ex: simple capture
  C.Capture('[Time hh-mm-ss]') %capture (use time tag as filename)
  file = C.lastfile %get last downloaded filenames
  
-Ex: timed capture
- C = CameraController;
- time = ceil(now*24*60*6)/24/60/60; %upcoming whole second
- file = [datestr(time,'yyyy-mm-dd_HHMMSS.FFF') '_' C.property.devicename]; %timestamp & camera name
- C.Capture(file,time); %capture
- datestr(time)
- 
 Ex: two cameras
  C = CameraController;
- C.session.filenametemplate = 'MyTest_[Camera Name]'; %set filename pattern
- C.Cameras(1), C.property.devicename = 'Cam1'; %set camera name (optional)
+ C.session.filenametemplate = '[Camera Name]'; %set filename pattern
+ C.Cameras(1), C.property.devicename = 'Cam1'; %change camera name
  C.Cameras(2), C.property.devicename = 'Cam2';
  C.Cmd('CaptureAll')
  
-Ex: focus stacking
- C = CameraController;
- C.Cmd('LiveViewWnd_Show'), pause(1) %turn on live preview
- for k = 0:2 %take 3 photos
- C.Focus(-2,'small',1) %two small step towards near focus
- C.Capture(num2str(k,'Focus%g')); %capture and number the photos
- end
- C.Cmd('LiveViewWnd_Hide') %turn off live preview to save battery
- 
-Ex: stream live view
-To remove rectangle: Live View>Display>Show focus rectangle
-To reduce lag enable: Live View>Display>No processing
- C = CameraController;
- C.Cmd('LiveViewWnd_Show'); %start live view
- C.Cmd('All_Minimize'); %minimise digiCamControl
- pause(3) %wait for live view
- clf, h = imshow(C.LiveView); %prepare figure
- uicontrol('str','Capture','call','C.Capture') %capture button
- while ishandle(h) %loop until closed
- set(h,'cdata',C.LiveView) %update live view
- drawnow %update display
- end
- C.Cmd('LiveViewWnd_Hide'); %stop live view
- 
-Ex: debugging
- C = CameraController;
- C.Clock %show a clock with milliseconds
- C.dbg = 2; %display commands and replies
- C.Capture %capture photo
- 
 Serge 2017
- Email questions/bugs/fixes to: s3rg3y@hotmail.com
+ Questions/bugs/fixes: s3rg3y@hotmail.com
+ 
